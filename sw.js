95437f3ab8eb031a98a1bc8c302a34735021922f
tiendungtcu <encorageugly@gmail.com>
2020-04-29 15:24:02 +0700

    update site

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8749f85242b36f7fe61b.js"
+    "url": "webpack-runtime-ec0c9ae24f03dc070b48.js"
   },
   {
     "url": "framework-727eca7a674e10b21a9f.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e34c8e3c73bb2fae55d060929c11cdcc"
+    "revision": "87aaaa2f42a78718dfead1ad9cdbcfc4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-2d6acd98ade61427dc4f.js"
